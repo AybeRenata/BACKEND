0ac062b06248c0a8d04522feae1a80ffb5434f86
AybeRenata <ayberenatalucia12@gmail.com>
2024-11-27 16:19:25 -0300

    importar libreria de cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = 3000;
 const { createClient } = require('@supabase/supabase-js');
 const { SUPABASE_URL, SUPABASE_KEY } = require("./config");
