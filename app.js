afd9fd242345cffdde896e197e99b3b2a9c6a66d
AybeRenata <ayberenatalucia12@gmail.com>
2024-12-04 22:42:31 -0300

    validation (#7)

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000;
 const { createClient } = require('@supabase/supabase-js');
 const { SUPABASE_URL, SUPABASE_KEY } = require("./config");
 const bodyParser = require("body-parser");
+const { productsValidation } = require("./middlewares");
+const { name, price, description, stock, brand, spec, store_id } = require("./validation");
 
 const supabase = createClient(
     SUPABASE_URL,
@@ -18,6 +20,20 @@ app.use(bodyParser.json());
 // trae todos los productos de una tienda
 app.get("/products", async (req, res) => {
     const store_id = req.query.store_id
+    if (!store_id) {
+        res.status(500).json("store_id es obligatorio en la query")
+    }
+
+    const { data: stores } = await supabase
+        .from('stores')
+        .select('*')
+        .eq("id", store_id)
+
+    if (stores.length === 0) {
+        res.status(500).json("No existe una tienda con ese id")
+    }
+
+
     const order = req.query.order
     const { data, error } = await supabase
         .from('products')
@@ -30,11 +46,10 @@ app.get("/products", async (req, res) => {
     }
     res.status(200).json(data)
 
-
-
 });
 
-
+// verificar que tenga un id [x]
+// verificar que ese id exista en la base de datos []
 
 // trae un producto por id
 app.get("/products/:id", async (req, res) => {
@@ -43,6 +58,11 @@ app.get("/products/:id", async (req, res) => {
         .from('products')
         .select('*')
         .eq('id', params.id)
+
+    if (data.length === 0) {
+        res.status(500).json("No existe un producto con ese id")
+    }
+
     if (error) {
         res.status(500).json("falló el get")
 
@@ -52,7 +72,7 @@ app.get("/products/:id", async (req, res) => {
 
 
 // crea un producto de una tienda
-app.post("/products", async function (req, res) {
+app.post("/products", [name, price, description, stock, brand, spec, store_id, productsValidation], async function (req, res) {
     const { name, price, description, stock, brand, spec, store_id } = req.body
 
     const { data, error } = await supabase
@@ -68,9 +88,8 @@ app.post("/products", async function (req, res) {
     res.status(200).json("post exitoso")
 });
 
-
 // actualiza un producto 
-app.put("/products/:id", async function (req, res) {
+app.put("/products/:id", [name, price, description, stock, brand, spec, store_id, productsValidation], async function (req, res) {
     const params = req.params
     const body = req.body
     const { data, error } = await supabase
@@ -93,7 +112,6 @@ app.delete("/products/:id", async function (req, res) {
         .from('products')
         .delete()
         .eq('id', params.id)
-    res.send("DELETE Request");
     if (error) {
         res.status(500).json("falló el delete")
 
